3f9eca97f17c06fd589081475c902bb9a25a3262
allandereal <ahullan@gmail.com>
2018-02-17 19:36:26 +0300

    test cronjob phase 2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use App\Conversations\Followup;
 use App\FbUser;
 use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\BotMan\Facades\BotMan;
 use BotMan\Drivers\Facebook\FacebookDriver;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -34,12 +35,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            $unfollowed_users = DB::select('SELECT * FROM fb_users WHERE followed IS NULL AND created_at > (NOW() - 7 days)');
+            $unfollowed_users = DB::select('SELECT * FROM fb_users WHERE DATEDIFF(NOW(),created_at)<10');
             if (count($unfollowed_users)>0){
                 foreach ($unfollowed_users as $u_user){
                     $botman = resolve('botman');
                     $botman->say('Thank you for using the HIV Self Testing ChartBot.', $u_user->user_id, FacebookDriver::class);
                     $botman->startConversation(new Followup($botman), $u_user->user_id, FacebookDriver::class);
+                    $botman = new BotMan;
+                    $botman->say('Thank you for using the HIV Self Testing ChartBot2.', $u_user->user_id, FacebookDriver::class);
                 }
             }
         })->everyMinute();
