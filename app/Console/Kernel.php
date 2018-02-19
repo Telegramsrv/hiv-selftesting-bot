1fd2519cc9cab3e75e20fcc42f238c3ac2ad368d
allandereal <ahullan@gmail.com>
2018-02-19 09:52:26 +0300

    adjust followup time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,15 +35,15 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            $unfollowed_users = DB::select('SELECT * FROM fb_users WHERE DATEDIFF(NOW(),created_at)<10');
+            $unfollowed_users = DB::select('SELECT * FROM fb_users WHERE DATEDIFF(NOW(),created_at)=7 AND followed=0');
             if (count($unfollowed_users)>0){
                 $botman = resolve('botman');
                 foreach ($unfollowed_users as $u_user){
-                    $botman->say('Thank you for using the HIV Self Testing ChartBot.', $u_user->user_id, FacebookDriver::class);
+                    $botman->say('Thank you for using the HIV Self Testing ChatBot.', $u_user->user_id, FacebookDriver::class);
                     $botman->startConversation(new TestFollowup($botman), $u_user->user_id, FacebookDriver::class);
                 }
             }
-        })->everyFiveMinutes();
+        })->daily();
     }
 
     /**
